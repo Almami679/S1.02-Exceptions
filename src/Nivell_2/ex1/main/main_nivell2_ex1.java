c1b31eb95693554b2aea2ec5d509a17dd6a5fd64
Almami989 <almami989@gmail.com>
2024-10-01 23:44:38 +0200

    Ejercicio nivel 2 a medias, despues de buscar por internet encontre el uso del hasNext, tampoco entiendo muy bien el enunciado del ejercicio, esto es lo que he podido desarrollar

--- src/Nivell_2/ex1/main/main_nivell2_ex1.java
+++ src/Nivell_2/ex1/main/main_nivell2_ex1.java
@@ -7,116 +7,29 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class main_nivell2_ex1 {
-    public static void main(String[] args) {
-
-        byte edad = llegirByte("Introduce tu edad");
-        int numero = llegirInt("Teclea el numero que aparece en la casilla [5]");
-        float medida = llegirFloat("Escribe en valor de coma flotante canto mides");
-        double numPi = llegirDouble("Escribe el numero PI");
-        char simbolo = llegirChar("Escribe el simbolo del Hastag");
-        String palabra = llegirString("Escribe una palabra");
-
-    }
-
-
-       // Mètodes a implantar capturant l’excepció de la classe "InputMismatchException":
-
-        public static byte llegirByte(String missatge) throws InputMismatchException {
-            byte respuesta = 0;
-            Scanner input = new Scanner(System.in);
-            System.out.println(missatge);
-            try {
-                respuesta = input.nextByte();
-
-            } catch(InputMismatchException e){
-                System.out.println("Formato de respuesta no compatible");
-                }
-            return respuesta;
-        }
-
-        public static int llegirInt(String missatge) throws InputMismatchException {
-            int respuesta = 0;
-            Scanner input = new Scanner(System.in);
-            System.out.println(missatge);
-            try {
-                respuesta = input.nextInt();
-            } catch(InputMismatchException e) {
-                System.out.println("Formato de respuesta no compatible");
-            }
-            return respuesta;
-        }
-
-        public static float llegirFloat(String missatge) throws InputMismatchException {
-            float respuesta = 0;
-            Scanner input = new Scanner(System.in);
-            System.out.println(missatge);
-            try {
-                respuesta = input.nextFloat();
-            } catch(InputMismatchException e) {
-                System.out.println("Formato de respuesta no compatible");
-            }
-            return respuesta;
-
-
-        }
-
-        public static double llegirDouble(String missatge) throws InputMismatchException {
-            double respuesta = 0;
-            Scanner input = new Scanner(System.in);
-            System.out.println(missatge);
-            try {
-                respuesta = input.nextDouble();
-            } catch(InputMismatchException e) {
-                System.out.println("Formato de respuesta no compatible");
-            }
-            return respuesta;
-        }
-
-
-        // Mètodes a implantar capturant una excepció personalitzada de la classe Exception:
-
-        public static char llegirChar(String missatge) throws Exception {
-            char respuesta = 0;
-            Scanner input = new Scanner(System.in);
-            System.out.println(missatge);
-            try {
-                respuesta = input.next();
-            } catch(Exception e) {
-                System.out.println("Formato de respuesta no compatible");
-            }
-            return respuesta;
-        }
-
-        public static String llegirString(String missatge) throws Exception {
-            new List<String> numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-            String respuesta = "";
-            Scanner input = new Scanner(System.in);
-            System.out.println(missatge);
-            try {
-                respuesta = input.next();
-            } catch(Exception e) {
-                System.out.println("Formato de respuesta no compatible");
-            }
-            return respuesta;
-
-        }
-
-        public static boolean llegirSiNo(String missatge) throws Entrada {
-            boolean respuesta = false;
-            Scanner input = new Scanner(System.in);
-            System.out.println(missatge);
-            try {
-                if (input.next().equalsIgnoreCase("s")) {
-                    respuesta = true;
-                } else if(input.next().equalsIgnoreCase("n")) {
-                    respuesta = false;
-                }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-            return respuesta;
-        } // si l’usuari/ària introdueix “s”, retorna
-                                                           // “true”, si l’usuari/ària introdueix “n”,
-                                                           // retorna “false”.
-
+    public static void main(String[] args) throws Exception {
+    	
+    	
+    	byte edad = Entrada.llegirByte("Introduce tu edad");
+    	System.out.println("Edad: " + edad);
+        
+        int numero = Entrada.llegirInt("Introduce el numero encasillado [5]");
+        System.out.println("Numero: " + numero);
+        
+        float altura = Entrada.llegirFloat("Introduce tu altura");
+        System.out.println("Altura: " + altura);
+        
+        double numeroPi = Entrada.llegirDouble("Introduce el numero PI");
+        System.out.println("Numero PI = " + numeroPi);
+        
+        char caracter = Entrada.llegirChar("Introduce un caracter");
+        System.out.println("Caracter introducido: " + caracter);
+        
+        String palabra = Entrada.llegirString("Introduce una palabra");
+        System.out.println("String introducida: " + palabra);
+        
+        boolean respuesta = Entrada.llegirSiNo("Estas de acuerdo?\n[S]í   [N]o");
+        System.out.println("Respuesta booleana: " + respuesta);
+        
+      }
 }
--- src/Nivell_2/ex1/modules/Entrada.java
+++ src/Nivell_2/ex1/modules/Entrada.java
@@ -1,12 +1,182 @@
 package Nivell_2.ex1.modules;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
-public class Entrada extends Exception{
 
-    private final static String message = "Formato de respuesta erroneo o no compatible";
+public class Entrada {
 
-    public Entrada() {
-        super(message);
+    
+    ////////////////////////////////////////////////////////////////////////
+    public static byte llegirByte(String missatge) throws InputMismatchException {
+        byte respuesta = 0;
+        Scanner input = new Scanner(System.in);
+        try{
+            System.out.println(missatge);
+            while (input.hasNext()) { 
+                
+            if (input.hasNextByte()) {
+                respuesta = input.nextByte();
+            } 
+            else {
+                System.out.println("No se ha introducido un byte: " + input.next()); 
+            } 
+        } 
+    } catch(InputMismatchException e) {
+        System.out.println("Formato de respuesta no compatible");
+    } catch(Exception e){
+        System.out.println("Error inesperado: " + e.getMessage());
+    }finally{
+        input.close();
     }
+    return respuesta;
+    }
+    
+    ////////////////////////////////////////////////////////////////////////
+    
+    public static int llegirInt(String missatge) throws InputMismatchException {
+        int respuesta = 0;
+        Scanner input = new Scanner(System.in);
+        try{
+            System.out.println(missatge);
+            while (input.hasNext()) { 
+                
+            if (input.hasNextInt()) {
+                respuesta = input.nextInt();
+            } 
+            else {
+                System.out.println("No se ha introducido un Integer: " + input.next()); 
+            } 
+        } 
+    } catch(InputMismatchException e) {
+        System.out.println("Formato de respuesta no compatible");
+    } catch(Exception e){
+        System.out.println("Error inesperado: " + e.getMessage());
+    }finally{
+        input.close();
+    }
+    return respuesta;
+    }
+    
+    ////////////////////////////////////////////////////////////////////////
+    
+    public static float llegirFloat(String missatge) throws InputMismatchException {
+    	float respuesta = 0;
+        Scanner input = new Scanner(System.in);
+        try{
+            System.out.println(missatge);
+            while (input.hasNext()) { 
+                
+            if (input.hasNextFloat()) {
+                respuesta = input.nextFloat();
+            } 
+            else {
+                System.out.println("No se ha introducido un float: " + input.next()); 
+            } 
+        } 
+    } catch(InputMismatchException e) {
+        System.out.println("Formato de respuesta no compatible");
+    } catch(Exception e){
+        System.out.println("Error inesperado: " + e.getMessage());
+    }finally{
+        input.close();
+    }
+    return respuesta;
+    }
+    
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    
+    public static double llegirDouble(String missatge) throws InputMismatchException {
+        double respuesta = 0;
+        Scanner input = new Scanner(System.in);
+        try{
+            System.out.println(missatge);
+            while (input.hasNext()) { 
+                
+            if (input.hasNextDouble()) {
+                respuesta = input.nextDouble();
+            } 
+            else {
+                System.out.println("No se ha introducido un double: " + input.next()); 
+            } 
+        } 
+    } catch(InputMismatchException e) {
+        System.out.println("Formato de respuesta no compatible");
+    } catch(Exception e){
+        System.out.println("Error inesperado: " + e.getMessage());
+    }finally{
+        input.close();
+    }
+    return respuesta;
+    }
+    
+    /////////////////////////////////////////////////////////////////////////////////////////////
+ // Mètodes a implantar capturant una excepció personalitzada de la classe Exception:
+
+    public static char llegirChar(String missatge) throws Exception {
+        char respuesta = ' ';//todas las char vacias se ponen en espacio blanco entre comillas simples
+        Scanner input = new Scanner(System.in);
+        try{
+            System.out.println(missatge);
+            while (input.hasNext()) { 
+                
+            if (inputNext().size(1)) {
+                respuesta = input.nextChar();
+            } 
+            else {
+                System.out.println("No se ha introducido un caracter: " + input.next()); 
+            } 
+        } 
+    } catch(Exception e){
+        System.out.println("Error inesperado: " + e.getMessage());
+    }finally{
+        input.close();
+    }
+    return respuesta;
+    }
+
+    public static String llegirString(String missatge) throws Exception {
+        String respuesta = "";
+        Scanner input = new Scanner(System.in); 
+        try{
+            System.out.println(missatge);
+            while (input.hasNext()) { 
+                
+            if (input.hasNextString()) {
+                respuesta = input.nextString();
+            } 
+            else {
+                System.out.println("No se ha introducido un Palabra: " + input.next()); 
+            } 
+        } 
+    } catch(Exception e){
+        System.out.println("Error inesperado: " + e.getMessage());
+    }finally{
+        input.close();
+    }
+    return respuesta;
+    }
+
+    public static boolean llegirSiNo(String missatge) throws Exception {
+        boolean respuesta = false;
+        Scanner input = new Scanner(System.in);
+        try {
+            System.out.println(missatge);
+            while (input.hasNext()) {
+
+                if (input.hasNext().equalsIgnoreCase("s")) {
+                    respuesta = true;
+                } else if (input.hasNext().equalsIgnoreCase("n")) {
+                    respuesta = false;
+                }
+            }
+        } catch(Exception e){
+            System.out.println("Error inesperado: " + e.getMessage());
+        }finally{
+            input.close();
+        }
+            return respuesta;
+
+    }
+            
 }
